cc82ef729105bdc539b3868a1351fe540062c9d8
CornHusker89 <cornhusker889@gmail.com>
2023-12-10 23:10:37 -0800

    added recieving commands with web api

--- web_api.py
+++ web_api.py
@@ -4,12 +4,31 @@ from flask_limiter import Limiter
 import os
 import asyncio
 import threading
+import traceback
 
-import minecraft_server
+from minecraft_server import server as start_minecraft_server, send_command
 
 
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+# get enviroment variables
+if not os.getenv("API_KEY"):
+    raise Exception("Please set the API_KEY environment variable")
+else:
+    api_key = os.getenv("API_KEY")
+
+if not os.getenv("HTTPS_CERTIFICATE_PATH"):
+    raise Exception("Please set the HTTPS_CERTIFICATE_PATH environment variable")
+else:
+    cert_path = os.getenv("HTTPS_CERTIFICATE_PATH")
+
+if not os.getenv("HTTPS_KEY_PATH"):
+    raise Exception("Please set the HTTPS_KEY_PATH environment variable")
+else:
+    key_path = os.getenv("HTTPS_KEY_PATH")
+
 
 # load flask
 def get_proxy_remote_address():
@@ -28,48 +47,55 @@ limiter = Limiter(
 )
 
 
-# get enviroment variables
-if not os.getenv("API_KEY"):
-    raise Exception("Please set the API_KEY environment variable")
-else:
-    api_key = os.getenv("API_KEY")
-
-
-
-
-
+#    if api_key == flask.request.headers.get("API_KEY"):
 
 # api routes
 @app.route('/api')
-def index():
+async def index():
     return flask.jsonify('API is online!')
 
 
-@app.route("/api/minecraft/start", methods=["POST"])
-def start():
+
+@limiter.limit("3/30 seconds")
+@app.route("/api/start", methods=["POST"])
+async def start():
 
     def start_server():
         asyncio.set_event_loop(asyncio.new_event_loop())
         loop = asyncio.get_event_loop()
-        loop.create_task(minecraft_server.server())
+        loop.create_task(start_minecraft_server())
         loop.run_forever()
         
     thread = threading.Thread(target=start_server)
     thread.start()
-    return "", 204
+    return flask.jsonify({}), 204
 
 
-@app.route("/api/minecraft/players", methods=["GET"])
-@limiter.limit("20/30 seconds")
-def names():
+
+@app.route("/api/command", methods=["POST"])
+@limiter.limit("50/30 seconds")
+async def command():
+    command = flask.request.json["command"]
+
+    try:
+        if type(command) == str:
+            command = [command]
+        response = await send_command(command)
+        return flask.jsonify({"command_output": response}), 200
+    
+    except Exception as e:
+        print(traceback.format_exc())
+        return flask.jsonify({"error": traceback.format_exc()}), 500
+
+
+@app.route("/api/players", methods=["GET"])
+@limiter.limit("15/30 seconds")
+async def names():
     data = {"names": ["test1", "test2", "test3"]}
     return flask.jsonify(data)
 
 
 
-    
-
-
 if __name__ == "__main__":
-    context = ('server_key/.crt', 'server_key/.key') # certificate and key files
+    context = (cert_path, key_path) # certificate and key files
     app.run(debug=True, port=3536, ssl_context=context)
